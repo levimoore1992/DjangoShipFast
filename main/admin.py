76b2cb51bc1c26aa50840538371c09aa144ff87e
Levi Moore <levimoore1992@gmail.com>
2023-10-08 16:17:47 +0800

    Issue #94: Redo some navbar styles with notifications

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
+from main.forms import NotificationAdminForm
 from main.models import Notification
 
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
+    form = NotificationAdminForm
     list_display = ("user", "message", "is_read", "created_at")
     list_filter = ("is_read", "created_at")
     search_fields = ("user", "message")
