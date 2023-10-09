a345ef1d32f7fbbd3581a10d2c2bb1b7c6a5eac6
Levi Moore <levimoore1992@gmail.com>
2023-10-09 11:11:50 +0800

    Issue #99: Create Terms and Conditions

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,11 @@ from .views import HomeView, MarkAsReadAndRedirectView, TermsAndConditionsView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path("terms-and-conditions/", TermsAndConditionsView.as_view(), name="terms_and_conditions"),
+    path(
+        "terms-and-conditions/",
+        TermsAndConditionsView.as_view(),
+        name="terms_and_conditions",
+    ),
     # Notification views
     path(
         "mark_as_read_and_redirect/<int:notification_id>/<path:destination_url>/",
