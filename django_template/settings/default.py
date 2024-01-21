b7ae6a94950c3f6ce12065f74883a26a69fd2d4e
Levi Moore <levimoore1992@gmail.com>
2024-01-21 21:02:29 +0900

    Issue#80: Lint

--- django_template/settings/default.py
+++ django_template/settings/default.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'waffle.middleware.WaffleMiddleware',
+    "waffle.middleware.WaffleMiddleware",
     "main.middleware.NotificationMiddleware",
 ]
 
