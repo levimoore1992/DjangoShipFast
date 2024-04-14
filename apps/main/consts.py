560747fbad2850d01703100c941f501781e40195
Levi Moore <levimoore1992@gmail.com>
2024-04-14 17:16:37 +0900

    Update consts.py

--- apps/main/consts.py
+++ apps/main/consts.py
@@ -43,5 +43,6 @@ class ContactType(Enum):
         """
         return [(key.value, key.value) for key in cls]
 
+
 # Classes for attaching to fields in any form that uses our normal styles
 FORM_CLASSES = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
