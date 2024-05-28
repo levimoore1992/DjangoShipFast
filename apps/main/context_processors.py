f60252fd72764b7827236cab99c566d76a646cd8
Levi Moore <levimoore1992@gmail.com>
2024-05-28 11:19:47 +0900

    Issue#233: Refactor notifications to be made in context processor

--- apps/main/context_processors.py
+++ apps/main/context_processors.py
@@ -12,3 +12,10 @@ def report_form(request):
         dict: A dictionary with the report form instance.
     """
     return {"report_form": ReportForm()}
+
+
+def notifications(request):
+    """A context processor that provides the user's notifications to all templates."""
+    if request.user.is_authenticated:
+        return {"notifications": request.user.notifications.filter(is_read=False)}
+    return {}
