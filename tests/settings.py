8b56b270d7c2f6c6174b1703af072f27ec2110fc
Levi Moore <levimoore1992@gmail.com>
2024-06-10 18:03:38 +0900

    Issue#246: Add Media Library Mixin

--- tests/settings.py
+++ tests/settings.py
@@ -2,6 +2,7 @@ from django_template.settings.default import *  # noqa: F401,F403
 
 ALLOWED_HOSTS = ["localhost", "testserver", "127.0.0.1", ".ngrok.io"]
 
+INSTALLED_APPS.append("tests.test_app")  # noqa: F405
 
 DATABASES = {
     "default": {
