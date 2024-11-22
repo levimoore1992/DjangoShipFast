5a2a0a8be833016060ef0fc429a6918b63578cb7
Levi Moore <levimoore1992@gmail.com>
2024-11-22 18:11:01 +0900

    Issue#324: Refactor Blocked Items

--- tests/users/test_admin.py
+++ tests/users/test_admin.py
@@ -66,9 +66,6 @@ class UserAdminTest(TestCase):
         # Check if the user's devices are blocked
         self.assertTrue(device.is_blocked, "User's device should be marked as blocked")
 
-        # Check if the user's IPs are marked as suspicious
-        self.assertTrue(ip.is_suspicious, "User's IP should be marked as suspicious")
-
 
 class UserIPAdminTest(TestCase):
     """
