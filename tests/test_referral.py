84b0ea7bb951ec870aee611b730acdb7534ff92c
Levi Moore <levimoore1992@gmail.com>
2026-01-05 14:28:22 +1100

    add test

--- tests/test_referral.py
+++ tests/test_referral.py
@@ -56,3 +56,14 @@ class ReferralSourceTests(TestCase):
             self.url, data=json.dumps(data), content_type="application/json"
         )
         self.assertNotEqual(response.status_code, 200)  # Should redirect or 403
+
+    def test_update_referral_source_invalid_data(self):
+        """
+        Test updating the referral source with invalid data.
+        """
+        self.client.login(email="test@example.com", password="password")
+        data = {}
+        response = self.client.post(
+            self.url, data=json.dumps(data), content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400)
